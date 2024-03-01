508635a73ce366117e073cef57963c0794c8ab5d
Jiahua Jiang <jiahua212@gmail.com>
2024-03-01 11:20:20 -0800

    working arrow keys and wasd

--- Lab 5 Moving Character/Scenes/main_character.gd
+++ Lab 5 Moving Character/Scenes/main_character.gd
@@ -26,11 +26,16 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var xdirection = Input.get_axis("input_left", "input_right")
+	var ydirection = Input.get_axis("input_up", "input_down")
+	if xdirection:
+		velocity.x = xdirection * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
 	
