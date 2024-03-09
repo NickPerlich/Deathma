9cc6fbfde808ae7a7bac2f80ebb22fea668b0554
NickPerlich <nicholasperlich2003@gmail.com>
2024-03-09 10:58:42 -0800

    player and enemy projectiles have collision resolution with eachother

--- Lab 5 Moving Character/Scenes/main_character.gd
+++ Lab 5 Moving Character/Scenes/main_character.gd
@@ -116,3 +116,7 @@ func _on_tongue_collected(texture, collisionShape, rect_region):
 	bulletList.append(bullet)
 
 	pass # Replace with function body.
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("enemy_projectile"):
+		print("player damaged")
--- Lab 5 Moving Character/Scripts/projectile_behavior.gd
+++ Lab 5 Moving Character/Scripts/projectile_behavior.gd
@@ -7,6 +7,7 @@ func _physics_process(delta):
 	position.x += delta  * horizontalVelocity
 	position.y += delta * verticalVelocity
 	
+
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("player"):
-		queue_free() # Replace with function body.
+		queue_free()
