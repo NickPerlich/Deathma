9d54b5403922211dee5501e7fe3aec89daa7f931
NickPerlich <nicholasperlich2003@gmail.com>
2024-03-17 14:28:56 -0700

    enemy is visibly damaged

--- Lab 5 Moving Character/Scripts/enemy.gd
+++ Lab 5 Moving Character/Scripts/enemy.gd
@@ -1,29 +1,42 @@
 extends CharacterBody2D
 
-var projectile = preload("res://Characters/spit_projectile.tscn")
-var projectileSpeed = 100
-var counter = 0
-@export var moveSpeed = 80
+@export var moveSpeed: int = 80
 @export var player: Node2D
-@onready var nav_agent = $NavigationAgent2D
-@onready var sprite_2d = $Sprite2D
 
-var health = 1
+var projectile: PackedScene = preload("res://Characters/spit_projectile.tscn")
+var projectileSpeed: int = 100
+var counter: int = 0
+var health: int = 2
 
-# moves the enemy in a direction that is updated by the navigation agent
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+@onready var sprite_color: Color = $Sprite2D.modulate
+@onready var damage_visual_timer: Timer = $DamageVisualTimer
+
+# BUILT IN VIRTUAL METHODS
+
 func _physics_process(_delta: float) -> void:
 	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	#only have enemy chase player if within a certain distance
 	if nav_agent.target_position.distance_to(position) > 700:
 		velocity = Vector2(0,0)
 	else:
 		velocity = direction * moveSpeed
 	sprite_2d.play("enemy-l")
 	move_and_slide()
-	
+
+# MOVEMENT RELATED METHODS
+
 # updates the target position of the navigation agent
 func update_path() -> void:
 	nav_agent.target_position = player.global_position
-	#print(player.global_position)
+	
+# on timer timeout the enemy updates the direction of its path toward the player
+func _on_path_timer_timeout():
+	update_path()
+	
+# COMBAT RELATED METHODS
+	
 # instantiates a projectile and initializes its speed and size
 func inst(projectileVelocity: Vector2):
 	var projectileInstance = projectile.instantiate()
@@ -37,18 +50,27 @@ func fire(speed):
 	inst(Vector2(0, -speed))
 	inst(Vector2(speed, 0))
 	inst(Vector2(-speed, 0))
-	
-# on timer timeout the enemy updates the direction of its path toward the player
-func _on_path_timer_timeout():
-	update_path()
 
 # on timer timeout the enemy fires projectiles
 func _on_fire_timer_timeout():
 	fire(projectileSpeed)		
 
+#HEALTH RELATED METHODS
+
+func _on_damage_visual_timer_timeout():
+	sprite_2d.modulate = sprite_color
+
+func visualize_damage():
+	sprite_2d.modulate = Color(1, 0, 0)
+	damage_visual_timer.start()
+
+# take damage
 func get_damaged():
+	visualize_damage()
 	if health == 0:
+		await damage_visual_timer.timeout
 		queue_free()
 	else:
 		health -= 1
+		
 		
